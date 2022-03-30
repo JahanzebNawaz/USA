e926b394cf62138d381c49afba57c1aa66023dea
jahanzebNawaz <jahanzaib.ch9996@gmail.com>
2022-03-30 12:10:27 -0400

    class methods and modules

---
+++ WEEK18/student.js
@@ -0,0 +1,25 @@
+
+// export const person_name = "John";
+
+
+// const myFunc = () => {
+
+//     const person_name = "John";
+//     console.log(person_name);
+//     return person_name;
+// }
+
+
+// export default myFunc;
+
+
+const Navbar = () => {
+
+
+    return ( <div>
+        <h1>THIS IS NAVBAR</h1>
+    </div>)
+}
+
+
+export default Navbar;
